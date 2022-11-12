0ccb689640e8389532ffa52394f16bc9264519c1
andiaryansyah <aariansyah70@gmail.com>
2022-11-12 20:41:27 +0800

    first commit

---
+++ src/constants/images.js
@@ -0,0 +1,10 @@
+import formatPDF from '../assets/pdf.svg'
+import formatJPG from '../assets/jpeg.svg'
+import PasBandara from '../assets/pas_bandara.webp'
+
+// eslint-disable-next-line
+export default {
+    formatJPG,
+    formatPDF,
+    PasBandara,
+} 
